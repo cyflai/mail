6e12a0cb63e55e8fae4ec396dec0323424bec561
cyflai <chrisyflai@gmail.com>
2023-10-20 13:31:33 +0000

    Create gpu-pool.sh

---
+++ gpu-pool.sh
@@ -0,0 +1,9 @@
+wget https://download.abelian.info/release/pool/abelminer-linux-amd64-v2.0.2.tar.gz
+
+tar xvf abelminer-linux-amd64-v2.0.2.tar.gz
+
+mv abelminer-linux-amd64-v2.0.2 pool
+
+cd pool
+
+./abelminer -U -P stratums://55b53d31bcf0b0970b68c3b040e4351be523d9ef00c3e94b919e1820266810df:LkdJkzcWChA8@gpool-service-dior.abelian.info:27778
